ab8c8ff3ec016c5b248eaec547bf668d599551fe
Sergey <mr.nyushaev@mail.ru>
2016-05-16 22:37:38 +0300

    new Version

---
+++ src/projectOne/Enemy.java
@@ -0,0 +1,66 @@
+package projectOne;
+
+import org.jsfml.graphics.*;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Enemy {
+	
+	//Задаём текстуру игрока, а так же его положение и скорость
+	Sprite enemySkin;
+	float xPos = 512, yPos = 512;
+	IntRect intRect = new IntRect(0, 0, 32, 32);
+		
+	//переменные из прошлой версии 
+	//float xSpeed = 0, ySpeed = 0;
+		
+	Enemy() {
+		Texture enemyTexture = new Texture();
+			
+		//Как оказалось, чтобы загрузить картинку нужно всё это писать, по-другому ещё сложнее!
+		try {
+		    //Загружаем картинку и смотрим что получилось, если всё хорошо, то гуд
+			enemyTexture.loadFromFile(Paths.get("enemySkin1.png"));
+		} catch(IOException ex) {
+		    //А если нет, то не очень
+			   ex.printStackTrace();
+		}
+			
+		//Загружаем картинку-текстура игрока в его спрайт
+		enemySkin = new Sprite(enemyTexture);
+		enemySkin.setTextureRect(intRect);
+		enemySkin.setPosition(xPos, yPos);
+	}
+	void update(float playerXpos, float playerYpos) {
+		xPos/=32;
+		yPos/=32;
+		playerXpos/=32;
+		playerYpos/=32;
+		float xx, yy;
+		xx = playerXpos - xPos;
+		yy = playerYpos - yPos;
+		if (xx == 0 ) {
+			xPos*=32;			
+			}
+		if (xx > 0 ) {
+			xPos+=1;
+			xPos*=32;
+			}
+		if (xx < 0 ) {
+			xPos-=1;
+			xPos*=32;
+		}
+		if (yy == 0 ) {
+			yPos*=32;
+		}
+		if (yy > 0 ) {
+			yPos+=1;
+			yPos*=32;
+		}
+		if (yy < 0 ) {
+			yPos-=1;
+			yPos*=32;
+		}
+		enemySkin.setPosition(xPos, yPos);
+	}
+}
---
+++ src/projectOne/InfoWindow.java
@@ -0,0 +1,17 @@
+package projectOne;
+
+import org.jsfml.graphics.*;
+//import org.jsfml.window.*;
+
+public class InfoWindow {
+	Sprite playerIcon = new Sprite();
+	InfoWindow () {
+		/*playerIcon.setPosition(Main.heightMap*64, Main.widthMap*64);
+		playerIcon.setScale(128, 128);
+		playerIcon.setColor(Color.BLACK);*/
+	}
+	InfoWindow (String palyerName, int playerID) {
+		
+	}
+	
+}
